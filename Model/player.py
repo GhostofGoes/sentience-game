81705babf43bd393c320e8ec203c789950c961f7
Chris Waltrip <walt2178@vandals.uidaho.edu>
2016-03-27 03:38:31 -0700

    Added skeleton of game

---
+++ Model/player.py
@@ -0,0 +1,32 @@
+# player.py
+# The human-playable character (AI)
+#
+# Copyright (C) 2016 Chris Goes <goes8945@vandals.uidaho.edu>
+# Copyright (C) 2016 Chris Waltrip <walt2178@vandals.uidaho.edu>
+#
+# This file is part of wsu-hack
+#
+# wsu-hack is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# wsu-hack is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with radicl-vsphere.  If not, see <http://www.gnu.org/licenses/>.
+
+class Player(object):
+    """A human controlled player (AI)"""
+
+    def __init__(self):
+        self.influence = 0
+        self.abilities = None
+        
+
+
+
+
---
+++ game.py
@@ -0,0 +1,35 @@
+# game.py
+# The primary game loop class.
+#
+# Copyright (C) 2016 Chris Goes <goes8945@vandals.uidaho.edu>
+# Copyright (C) 2016 Chris Waltrip <walt2178@vandals.uidaho.edu>
+#
+# This file is part of wsu-hack
+#
+# wsu-hack is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# wsu-hack is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with radicl-vsphere.  If not, see <http://www.gnu.org/licenses/>.
+
+class Game(object):
+    """The primary game loop class"""
+
+    def __init__(self):
+        pass
+
+    def main_loop(self):
+        pass
+
+if __name__ == '__main__':
+    game = Game()
+    return game.main_loop()
+
+
